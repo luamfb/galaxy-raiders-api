ede454be9c2dfa5957fe77794b7f2ee8221fbc7f
luamfb <luamfb@riseup.net>
2022-07-18 14:15:28 -0300

    Vector2D: Implement dot product

--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -33,7 +33,7 @@ data class Vector2D(val dx: Double, val dy: Double) {
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    return dx * v.dx + dy * v.dy
   }
 
   operator fun plus(v: Vector2D): Vector2D {
