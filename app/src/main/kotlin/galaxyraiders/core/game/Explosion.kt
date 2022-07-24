bea2be5d4795b9e4ce9317d4f1f110e0384ccf64
luamfb <luamfb@riseup.net>
2022-07-23 23:52:47 -0300

    Create class Explosion

---
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -0,0 +1,7 @@
+package galaxyraiders.core.game
+
+import galaxyraiders.core.physics.Point2D
+import galaxyraiders.core.physics.Vector2D
+
+class Explosion(initialPosition: Point2D, radius: Double) :
+  SpaceObject("Explosion", '*', initialPosition, Vector2D(0.0, 0.0), radius, 0.0)
--- app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/SpaceField.kt
@@ -38,8 +38,11 @@ data class SpaceField(val width: Int, val height: Int, val generator: RandomGene
   var asteroids: List<Asteroid> = emptyList()
     private set
 
+  var explosions: List<Explosion> = emptyList()
+    private set
+
   val spaceObjects: List<SpaceObject>
-    get() = listOf(this.ship) + this.missiles + this.asteroids
+    get() = listOf(this.ship) + this.missiles + this.asteroids + this.explosions
 
   fun moveShip() {
     this.ship.move(boundaryX, boundaryY)
